9ae5c98902cbbd42a7f47881a7763ac71538676c
In-hyuk <dlsgur109@naver.com>
2023-11-09 11:30:34 +0900

    바코드

---
+++ 바코드 생성기/바코드 생성기/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Printing;
+
+namespace 바코드_생성기
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            BarcodeLib.Barcode barcode = new BarcodeLib.Barcode();
+            pictureBox1.Image = barcode.Encode(BarcodeLib.TYPE.CODE128, textBox1.Text, Color.Red, Color.White);
+        }
+
+        private void dwn_btn_Click(object sender, EventArgs e)
+        {
+            string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), textBox1.Text);
+            pictureBox1.Image.Save(path + " .jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+        }
+
+        private void Prt_btn_Click(object sender, EventArgs e)
+        {
+            PrintDialog print = new PrintDialog();
+            System.Drawing.Printing.PrintDocument Doc = new System.Drawing.Printing.PrintDocument();
+
+            Doc.PrintPage += new PrintPageEventHandler(Doc_page);
+
+            if (print.ShowDialog() == DialogResult.OK)
+                Doc.Print();
+
+        }
+
+        private void Doc_page(object sender, PrintPageEventArgs e)
+        {
+            e.Graphics.DrawImage(pictureBox1.Image, pictureBox1.Width, pictureBox1.Height);
+        }
+
+    }
+}
