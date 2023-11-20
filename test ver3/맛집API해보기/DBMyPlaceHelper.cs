49c84ce63188b087560cf1ca0a8ff8260a740917
chjeong <144426107+HyojeongChoe@users.noreply.github.com>
2023-11-20 16:40:08 +0900

    하...

---
+++ test ver3/맛집API해보기/DBMyPlaceHelper.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 맛집API해보기
+{
+    public class DBMyPlaceHelper
+    {
+        private static SqlConnection conn = new SqlConnection();
+
+        // DB 연동하는 메소드
+        public static void ConnectDB()
+        {
+            string dataSource = "local";
+            string db = "User_info"; // DB명
+            string security = "SSPI";
+            conn.ConnectionString =
+                $"Data Source=({dataSource}); initial Catalog={db};" +
+                $"integrated Security={security};" +
+                "Timeout=3";
+
+            if (conn.State != ConnectionState.Open)
+                conn.Open();
+        }
+
+        public static void CloseConnection()
+        {
+            if (conn.State == ConnectionState.Open)
+                conn.Close();
+        }
+
+        public static void RegisterMyPlace(string userId, string bz_nm, string fd_cs, string gng_cs)
+        {
+            try
+            {
+                ConnectDB();
+                string insertQuery = $"INSERT INTO MyPlace (User_ID, BZ_NM, FD_CS, GNG_CS) VALUES (@UserId, @bz_nm, @fd_cs, @gng_cs)";
+                using (SqlCommand insertCommand = new SqlCommand(insertQuery, conn))
+                {
+                    insertCommand.Parameters.AddWithValue("@UserId", userId);
+                    insertCommand.Parameters.AddWithValue("@bz_nm", bz_nm);
+                    insertCommand.Parameters.AddWithValue("@fd_cs", fd_cs);
+                    insertCommand.Parameters.AddWithValue("@gng_cs", gng_cs);
+
+                    insertCommand.ExecuteNonQuery();
+                }
+            }
+            finally
+            {
+                CloseConnection();
+            }
+        }
+       
+        public static DataTable GetMyPlaceData(string userId)
+        {
+            ConnectDB();
+            try
+            {
+                string selectQuery = $"SELECT * FROM MyPlace where User_ID = '{userId}'";
+                using (SqlCommand selectCommand = new SqlCommand(selectQuery, conn))
+                {
+                    SqlDataAdapter adapter = new SqlDataAdapter(selectCommand);
+                    DataTable myPlaceData = new DataTable();
+                    adapter.Fill(myPlaceData);
+                   
+                    return myPlaceData;
+                }
+            }
+            finally
+            {
+                CloseConnection();
+            }
+        }
+
+    }
+}
---
+++ test ver3/맛집API해보기/MyPlace.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 맛집API해보기
+{
+    public partial class MyPlace : UserControl
+    {
+        public MyPlace()
+        {
+            InitializeComponent();
+        }
+
+        private void MyPlace_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = null; // 이전 데이터 지우기
+                                            
+            BindMyPlaceData(); // MyPlace 테이블을 조회하여 데이터를 DataGridView에 바인딩
+        }
+
+
+        private void BindMyPlaceData()
+        {
+            
+            try
+            {
+                // MyPlace 테이블의 데이터 가져오기
+                DataTable myPlaceData = DBMyPlaceHelper.GetMyPlaceData(DBUserHelper.GetCurrentUserId());
+                
+                // DataGridView에 데이터 바인딩
+                dataGridView1.DataSource = myPlaceData;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"데이터를 불러오는 중 오류가 발생했습니다: {ex.Message}", "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+
+}
+
