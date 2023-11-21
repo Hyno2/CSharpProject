15b8906d4589c66ecfd63aa9c8fe55d4af6d0698
jks92-bb <obhop92@gmail.com>
2023-11-21 11:50:43 +0900

    ESC update

---
+++ test ver3.5/맛집API해보기/Convenience.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 맛집API해보기
+{
+    public class Convenience
+    {
+        #region 폼 빠르게 닫기
+        public static void CloseFormOnEscape(KeyPressEventArgs e, Form form)
+        {
+            // ESC 키를 눌렀을 때
+            if (e.KeyChar == (char)27)
+            {
+                form.Close(); // 현재 폼 닫기
+            }
+        }
+        #endregion
+
+    }
+}
